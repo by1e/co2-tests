6faf8c07aa4e118fe65c619f23bbd64b834ffcff
bangquan.qian <chainz@foxmail.com>
2020-07-21 18:15:47 +0800

    Update MyTest003.java

--- src/test/java/cn/by1e/co2/tests/MyTest003.java
+++ src/test/java/cn/by1e/co2/tests/MyTest003.java
@@ -2,6 +2,8 @@ package cn.by1e.co2.tests;
 
 import cn.by1e.ox.core.util.ConsoleUtils;
 
+import java.math.BigDecimal;
+
 /**
  * @author bangquan.qian
  * @date 2020-07-15 16:08
@@ -13,8 +15,10 @@ public class MyTest003 {
         public void test() {
             double r1 = .1 + .2;
             float r2 = .2f + .2f;
+            BigDecimal r3 = BigDecimal.valueOf(.1).add(BigDecimal.valueOf(.2));
             ConsoleUtils.sout(r1);
             ConsoleUtils.sout(r2);
+            ConsoleUtils.sout(r3);
         }
 
     }
