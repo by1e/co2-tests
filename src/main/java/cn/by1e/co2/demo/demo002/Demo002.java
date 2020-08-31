8f42fa9254ee2a24e85a04931e4373f314f40a88
bangquan.qian <chainz@foxmail.com>
2020-08-31 13:08:25 +0800

    修改测试代码

---
+++ src/main/java/cn/by1e/co2/demo/demo002/Demo002.java
@@ -0,0 +1,41 @@
+package cn.by1e.co2.demo.demo002;
+
+import cn.by1e.ox.core.util.ConsoleUtils;
+
+/**
+ * @author bangquan.qian
+ * @date 2020-08-29 13:18
+ */
+public class Demo002 {
+
+    static class Car {
+        void drive() {
+            ConsoleUtils.sout("car drive");
+        }
+    }
+
+
+    static class BmwCar extends Car {
+        @Override
+        void drive() {
+            ConsoleUtils.sout("bmw ar drive");
+        }
+    }
+
+    static class AdiCar extends Car {
+        @Override
+        void drive() {
+            ConsoleUtils.sout("adi ar drive");
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.drive();
+        car = new BmwCar();
+        car.drive();
+        car = new AdiCar();
+        car.drive();
+    }
+
+}
